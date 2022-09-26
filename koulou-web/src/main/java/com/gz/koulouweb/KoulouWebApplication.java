a1e0418f75bc58c696372d49639e71fb3e6a4a62
wangjunfeng <1159622457@qq.com>
2022-09-26 16:54:31 +0800

    pom udpate

--- koulou-web/src/main/java/com/gz/koulouweb/KoulouWebApplication.java
+++ koulou-web/src/main/java/com/gz/koulouweb/KoulouWebApplication.java
@@ -1,8 +1,10 @@
 package com.gz.koulouweb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan()
 @SpringBootApplication
 public class KoulouWebApplication {
 
