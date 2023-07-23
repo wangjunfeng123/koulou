62e30562c0bba69b85d82fe35f436186111a348d
wangjunfeng <wangjunfeng@payeasenet.com>
2023-07-23 17:30:33 +0800

    1

---
+++ koulou-biz/src/main/java/com/gz/kouloubiz/KoulouBizApplication.java
@@ -0,0 +1,13 @@
+package com.gz.kouloubiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoulouBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoulouBizApplication.class, args);
+    }
+
+}
---
+++ koulou-payment/src/main/java/com/gz/kouloupayment/KoulouPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.gz.kouloupayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoulouPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoulouPaymentApplication.class, args);
+    }
+
+}
